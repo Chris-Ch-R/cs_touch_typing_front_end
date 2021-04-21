f1a418a1b3cc35bfc4c46337c67e4843194ee183
Chris-Ch-R <chayangkun.r@ku.th>
2021-04-22 00:38:46 +0700

    Add google login api success.1

---
+++ src/views/NewPage.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <GoogleLogin
+      :params="params"
+      :renderParams="renderParams"
+      :onSuccess="onSuccess"
+      :onFailure="onFailure"
+    ></GoogleLogin>
+  </div>
+</template>
+
+<script>
+import GoogleLogin from "vue-google-login";
+
+export default {
+  name: "App",
+  data() {
+    return {
+      params: {
+        client_id:
+          "971929386601-nr1h5oi81ve8jna4gvcoud318k6hs8lq.apps.googleusercontent.com",
+      },
+      // only needed if you want to render the button with the google ui
+      renderParams: {
+        width: 250,
+        height: 50,
+        longtitle: true,
+      },
+    };
+  },
+  components: {
+    GoogleLogin,
+  },
+  methods: {
+    onSuccess(googleUser) {
+      console.log(googleUser);
+
+      // This only gets the user information: id, name, imageUrl and email
+      console.log(googleUser.getBasicProfile());
+    },
+    onFailure(errorData) {
+      console.log("ErrorMessage:" + errorData);
+    },
+  },
+};
+</script>
+
+<style></style>
